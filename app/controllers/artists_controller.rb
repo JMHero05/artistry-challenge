25db61f4443bd3b24ecc4d3e8656af7f6f84e29e
JMHero05 <jake.mills.05@gmail.com>
2020-08-11 12:31:50 -0400

    Lab Completed

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,2 +1,36 @@
 class ArtistsController < ApplicationController
+
+  def index
+    @artists = Artist.all
+  end
+
+  def show
+    find_artist
+  end
+
+  def new
+    @artist = Artist.new
+  end
+
+  def create
+    @artist = Artist.new(artist_params)
+
+    if @artist.valid?
+      @artist.save
+      redirect_to artist_path(@artist)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def artist_params
+    params.require(:artist).permit(:name, :age, :title)
+  end
+
+  def find_artist
+    @artist = Artist.find(params[:id])
+  end
+
 end
